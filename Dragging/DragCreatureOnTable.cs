27bab20bff8446910fd640190ded02a8f20ab679
سهيل هانى محمد صلاح الدين محمد فوزى <11410120170121@stud.cu.edu.eg>
2022-02-19 23:44:35 +0200

    first comment

---
+++ Dragging/DragCreatureOnTable.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class DragCreatureOnTable : DraggingActions {
+
+    private int savedHandSlot;
+    private WhereIsTheCardOrCreature whereIsCard;
+    private IDHolder idScript;
+    private VisualStates tempState;
+    private OneCardManager manager;
+
+    public override bool CanDrag
+    {
+        get
+        { 
+            // TEST LINE: this is just to test playing creatures before the game is complete 
+            // return true;
+
+            // TODO : include full field check
+            return base.CanDrag && manager.CanBePlayedNow;
+        }
+    }
+
+    void Awake()
+    {
+        whereIsCard = GetComponent<WhereIsTheCardOrCreature>();
+        manager = GetComponent<OneCardManager>();
+    }
+
+    public override void OnStartDrag()
+    {
+        savedHandSlot = whereIsCard.Slot;
+        tempState = whereIsCard.VisualState;
+        whereIsCard.VisualState = VisualStates.Dragging;
+        whereIsCard.BringToFront();
+
+    }
+
+    public override void OnDraggingInUpdate()
+    {
+
+    }
+
+    public override void OnEndDrag()
+    {
+
+        // 1) Check if we are holding a card over the table
+        TablePosition? tablePosition = DragSuccessful();
+        if (tablePosition != null)
+        {
+            // determine table position
+            int tablePos = playerOwner.PArea.tablesVisuals[(int)tablePosition]
+                .TablePosForNewCreature(Camera.main.ScreenToWorldPoint(
+                new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - Camera.main.transform.position.z)).x);
+            // Debug.Log("Table Pos for new Creature: " + tablePos.ToString());
+            // play this card
+            playerOwner.PlayACreatureFromHand(GetComponent<IDHolder>().UniqueID,
+                tablePos, (TablePosition)tablePosition);
+        }
+        else
+        {
+            // Set old sorting order 
+            whereIsCard.SetHandSortingOrder();
+            whereIsCard.VisualState = tempState;
+            // Move this card back to its slot position
+            HandVisual PlayerHand = playerOwner.PArea.handVisual;
+            Vector3 oldCardPos = PlayerHand.slots.Children[savedHandSlot].transform.localPosition;
+            transform.DOLocalMove(oldCardPos, 1f);
+        } 
+    }
+
+    protected override TablePosition? DragSuccessful()
+    {
+
+        TablePosition? PointedTable = TableVisual.CursorOverTable;
+        if (PointedTable == null) return null;
+        bool TableNotFull = (playerOwner.tables[(int)PointedTable].CreaturesOnTable.Count < 8);
+        return TableNotFull ? PointedTable : null;
+           
+    }
+}
